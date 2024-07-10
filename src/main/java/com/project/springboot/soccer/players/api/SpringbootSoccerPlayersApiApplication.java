c0c017136e54aa8be8119582a58ee0ffae5eb82f
Donovan Ramirez <donovanramirezc@hotmail.com>
2024-07-10 15:51:39 -0500

    Add initial configuration

---
+++ src/main/java/com/project/springboot/soccer/players/api/SpringbootSoccerPlayersApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.springboot.soccer.players.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoccerPlayersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSoccerPlayersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/springboot/soccer/players/api/SpringbootSoccerPlayersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springboot.soccer.players.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSoccerPlayersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
